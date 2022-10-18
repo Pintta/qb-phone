5fecc3321d801bab921aa6d165aa346ca7766512
Pintta <69728770+Pintta@users.noreply.github.com>
2022-10-19 02:37:56 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,12 +11,12 @@ shared_scripts {
 
 client_scripts {
     'client/main.lua',
-    'client/animation.lua'
+    'client/animation.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'server/main.lua',
 }
 
 files {
